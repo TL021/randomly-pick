801836ddadcb5fde88a0ad15715e80d7b285258d
Tian <tialiu2@cisco.com>
2022-04-29 17:25:45 +0800

    update data

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-export const employeeArr = ["Albert", "Angela", "Binary", "Caesar", "Calvin", "Carl", "Carrie", "Chris", "Daniel", "Eden", "Fei", "George", "Hedy", "Jackson", "Jayce", "Jimmy", "Kevin", "Mona", "Rocky", "Selly", "Ted", "Travis", "Vernon", "Yang"]
-export const employeeDailyArr = ["Albert", "Angela", "Binary", "Caesar", "Carl", "Carrie", "Chris", "Daniel", "Eden", "Fei", "George", "Hedy", "Jackson", "Jayce", "Jimmy", "Kevin", "Mona", "Rocky", "Selly", "Ted", "Travis", "Vernon", "Yang"]
+export const employeeArr = ["Albert", "Angela", "Binary", "Caesar", "Calvin", "Carl", "Chris", "Daniel", "Eden", "Fei", "George", "Hedy", "Jackson", "Jayce", "Jimmy", "Kevin", "Mona", "Rocky", "Scarlett", "Selly", "Ted", "Travis", "Vernon", "Yang"]
+export const employeeDailyArr = ["Albert", "Angela", "Binary", "Caesar", "Carl", "Chris", "Daniel", "Eden", "Fei", "George", "Hedy", "Jackson", "Jayce", "Jimmy", "Kevin", "Mona", "Rocky", "Scarlett", "Selly", "Ted", "Travis", "Vernon", "Yang"]
 
 export const employeeSet = new Set(employeeArr)
